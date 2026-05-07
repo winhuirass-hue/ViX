664df523e7989443a8e35743ca27ac2475cd0383
winhuirass-hue <winhuirass@gmail.com>
2026-05-07 16:45:44 +0300

    Add stddef.h with size_t and ptrdiff_t definitions

---
+++ src/kernel/include/stddef.h
@@ -0,0 +1,9 @@
+#ifndef _STDDEF_H
+#define _STDDEF_H
+
+typedef __SIZE_TYPE__ size_t;
+typedef __PTRDIFF_TYPE__ ptrdiff_t;
+#define NULL ((void *)0)
+#define offsetof(type, member) __builtin_offsetof(type, member)
+
+#endif
